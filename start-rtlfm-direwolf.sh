c72ad89951129ab29621ef776b742acb74b5e5d6
Masaru <masaruyokoi@gmail.com>
2024-06-26 23:37:14 +0900

    changed squelch level to 70

--- start-rtlfm-direwolf.sh
+++ start-rtlfm-direwolf.sh
@@ -7,6 +7,6 @@ direwolf_conf="$thisdir/direwolf.conf"
 pcm_rate=48000
 
 (
-  rtl_fm -M fm -f 144.64M -f 144.66M -f 431.04M -p 36 -s $pcm_rate  -l 20 -  | \
+  rtl_fm -M fm -f 144.64M -f 144.66M -f 431.04M -p 36 -s $pcm_rate  -l 70 -  | \
   tee >(direwolf -c "$direwolf_conf" -r $pcm_rate -D 1 -t 0 -B 1200 - | logger -t direwolf1)| \
   direwolf -c "$direwolf_conf" -r $pcm_rate -D 1 -t 0 -B 9600 - | logger -t direwolf9)  &
