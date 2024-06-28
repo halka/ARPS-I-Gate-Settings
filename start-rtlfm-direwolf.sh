00f7e3c65284f8e5647c330a7197e4170613ee2d
Masaru <masaruyokoi@gmail.com>
2024-06-28 12:02:20 +0900

    change sql level

--- start-rtlfm-direwolf.sh
+++ start-rtlfm-direwolf.sh
@@ -5,8 +5,9 @@
 thisdir="$(dirname $0)"
 direwolf_conf="$thisdir/direwolf.conf"
 pcm_rate=48000
+[ -z "$squelch_level" ] && squelch_level=70
 
 (
-  rtl_fm -M fm -f 144.64M -f 144.66M -f 431.04M -p 36 -s $pcm_rate  -l 70 -  | \
+  rtl_fm -M fm -f 144.64M -f 144.66M -f 431.04M -p 36 -s $pcm_rate  -l $squelch_level -  | \
   tee >(direwolf -c "$direwolf_conf" -r $pcm_rate -D 1 -t 0 -B 1200 - | logger -t direwolf1)| \
   direwolf -c "$direwolf_conf" -r $pcm_rate -D 1 -t 0 -B 9600 - | logger -t direwolf9)  &
