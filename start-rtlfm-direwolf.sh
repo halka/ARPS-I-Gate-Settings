5e81c82e5dbdc97d8f0679449dd0718f8b88703c
halka <halka@rjch.jp>
2024-12-15 19:37:02 +0900

    pll

--- start-rtlfm-direwolf.sh
+++ start-rtlfm-direwolf.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-# -l : Squelch Level
-# -g : Tuner Gain
-# -d : Dongle's Device Index or S/N
-# -a : Volume
-
 here="$(dirname $0)"
 direwolf_conf="$here/direwolf.conf"
 pcm_rate=48000
 device=00000001
 squelch_level=35
-
+gain=0
+pll=36
 
 [ -z "$squelch_level" ] && squelch_level=70
 
 (
-  rtl_fm -f 144.64M:144.66M:431.04M:431.09M -p 36 -s $pcm_rate -d $device -l $squelch_level -  | \
+  rtl_fm -f 144.64M:144.66M:431.04M:431.09M -p $pll -g $gain -s $pcm_rate -d $device -l $squelch_level -  | \
   tee >(direwolf -c "$direwolf_conf" -r $pcm_rate -D 1 -t 0 -B 1200 - | logger -t direwolf1)| \
   direwolf -c "$direwolf_conf" -r $pcm_rate -D 1 -t 0 -B 9600 - | logger -t direwolf9)  &
